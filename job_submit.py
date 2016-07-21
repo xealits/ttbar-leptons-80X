83f3645e6ad9e4ddd5d76bedd2936619f77959f5
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2016-07-21 17:56:00 +0100

    Temporary left here the 1 version of refactored job-submit script

---
+++ job_submit.py
@@ -0,0 +1,200 @@
+"""
+The script submitting jobs to LSF on lxplus.
+Takes as input:
+ * executable
+ * cfg template
+ * dsets.json
+
+Creates the job-farm directory.
+And launches the jobs.
+
+The job is:
+
+    exec cfg.py
+
+The `cfg.py` is compiled from `cfg template` and `dsets.json`.
+
+The job is submitted to LSF as a shell script `job.sh`.
+(Which contains `exec cfg.py` inside somewhere.)
+The submittion to the LSF job queue is done from shell with `bsub` command:
+
+    $ bsub ... job.sh
+
+
+# More info
+
+A job.sh looks like:
+    #! /bin/sh
+    pwd
+    export SCRAM_ARCH=slc6_amd64_gcc493
+    export BUILD_ARCH=slc6_amd64_gcc493
+    export VO_CMS_SW_DIR=/nfs/soft/cms
+    cd /afs/cern.ch/work/o/otoldaie/private/16/CMSSW_8_0_5/src/UserCode/llvv_fwk
+    eval `scramv1 runtime -sh`
+    cd -
+    ulimit -c 0;
+    ttbarleps80_eventSelection /afs/cern.ch/user/o/otoldaie/work/private/16/CMSSW_8_0_5/src/UserCode/llvv_fwk/data/output/ttbar-leps-80X_1//MC13TeV_TTJets_powheg_514_cfg.py
+
+
+Job submittion is done with:
+    bsub -q 8nh -R "pool>30000" -J ttbarleps80_eventSelection_MC13TeV_TTJets_powheg0000_ -oo /afs/cern.ch/user/o/otoldaie/work/private/16/CMSSW_8_0_5/src/UserCode/llvv_fwk/data/output/ttbar-leps-80X_1//FARM/outputs/0000_ttbarleps80_eventSelection_MC13TeV_TTJets_powheg.cout '/afs/cern.ch/user/o/otoldaie/work/private/16/CMSSW_8_0_5/src/UserCode/llvv_fwk/data/output/ttbar-leps-80X_1//FARM/inputs/job0000_ttbarleps80_eventSelection_MC13TeV_TTJets_powheg.sh'
+
+
+The cfg.template has the fields `@field` to fill in:
+    theLumiMask = path.expandvars(@lumiMask)
+    
+    runProcess = cms.PSet(
+        dtag  = cms.string("@dtag"),
+        job_num  = cms.string("@job_num"),
+        input = cms.untracked.vstring("@input"),
+        outfile = cms.string("@outfile"),
+
+--- TODO: make a pythonic template, with {} instead of the weird @foo.
+
+The dsets.json file structured as:
+    {
+      "proc":[
+    
+        {
+          "tag":"data",
+          "isdata":true,
+          "color":1,
+          "fill":0,
+          "comment":"real split 17",
+           "data":[
+    
+            { "dtag":"Data13TeV_SingleMuon2016B_PromptRecoV2", "split_NoUse_TestingAutomaticOne":50, "xsec":1.0, "br":[ 1.0 ],
+              "dset":"/SingleElectron/Run2016B-PromptReco-v2/MINIAOD",
+              "lumiMask":"/afs/cern.ch/user/o/otoldaie/work/private/project/CMSSW_7_6_3/src/UserCode/llvv_fwk/data/json/Cert_13TeV_16Dec2015ReReco_Collisions15_25ns_JSON_v2.txt" }
+    
+          ]
+        }
+    
+      ]
+    }
+
+
+"""
+
+#TODO: import argparse
+
+from sys import argv, exit
+import json, os
+import commands # for some reason subprocess.check_output does not work with das_client.py
+
+
+
+if len(argv) != 5:
+    print("Usage:\njob_submit.py executable_filename cfg.py_template_filename dsets.json outdirname")
+    exit(1)
+
+
+# Process input
+
+exec_name, cfg_templ_filename, dsets_json_filename, outdirname = argv[1:]
+outdirname = os.path.abspath(outdirname.strip()) + '/' # just in case
+
+with open(cfg_templ_filename) as t_f, open(dsets_json_filename) as d_f:
+    dsets = json.load(d_f)
+    cfg_templ = t_f.read()
+
+job_dir = outdirname + "/FARM/inputs/"
+job_outs = outdirname + "/FARM/outputs/"
+
+
+# Make job FARM directories
+
+os.makedirs(job_dir)
+os.makedirs(job_outs)
+project_dir = os.getcwd()
+
+# Job templates
+
+# TODO: add SCRAM_ARCH and other parameters
+job_template = """#!/bin/sh
+pwd
+export SCRAM_ARCH=slc6_amd64_gcc493
+export BUILD_ARCH=slc6_amd64_gcc493
+export VO_CMS_SW_DIR=/nfs/soft/cms
+cd {project_dir}
+eval `scramv1 runtime -sh`
+cd -
+ulimit -c 0;
+{exec_name} {job_cfg}
+"""
+
+job_command_template = """bsub -q 8nh -R "pool>30000" -J {job_name} -oo {job_stdout} '{jobsh}'"""
+
+# could use list comprehension instead of this
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+# Go through dsets.json
+# Create jobs.sh
+# Submit them
+
+n_files_per_job = 5
+
+for dset_group in dsets['proc']:
+    isdata = dset_group.get('isdata', False)
+    # TODO: other parameters?
+        # two required parameters of a dset
+    for dset in dset_group['data']:
+        dset = dset['dset']
+        dtag = dset['dtag']
+        lumiMask = dset.get('lumiMask', '')
+        # TODO: other parameters as well?
+
+        # get files of the dset
+        status, out = commands.getstatusoutput('das_client.py --limit=0 --query="file dataset={}"'.format(dset))
+        out_rows = out.split('\n')
+
+        if status != 0 or len(out_rows) < 4:
+            print("Failed fetch files of {} dataset".format(dset))
+            print("das_client.py status = " + str(status))
+            print("              output = " + out)
+            print("Continue to other dsets")
+            continue
+
+        dset_files = out_rows[3:]
+        print("Found {} files. Splitting {} per job.".format(len(dset_files), n_files_per_job))
+
+        # the list of bsubs job submition commands
+        dset_bsubs = []
+
+        # make jobs per file chunks
+        for i, job_chunk in enumerate(chunks(dset_files, n_files_per_job)):
+            job_name = exec_name + '_' + dtag + '_' + str(i) + '_'
+
+            job_cfg = cfg_templ.format(input = job_chunk, lumiCert = lumiMask, dtag = dtag, outdir = outdirname, jobID = i, isMC = not isdata, outfile = outdirname + '_' + str(i) + '.root')
+            # job_cfg = cfg_templ.format(job_files = job_chunk, lumiCert = lumiMask, dtag = dtag, outdir = outdirname, jobID = i, isdata = isdata)
+
+            job_cfg_filename = outdirname + dtag + '_' + str(i) + '_cfg.py'
+            with open(job_cfg_filename, 'w') as cfg_file:
+                cfg_file.write(job_cfg)
+
+            job_sh = job_template.format(project_dir = project_dir, exec_name = exec_name, job_cfg = job_cfg_filename)
+            job_sh_filename = outdirname + '/FARM/inputs/' + dtag + '_' + str(i) + '.sh'
+            with open(job_sh_filename, 'w') as sh_file:
+                sh_file.write(job_sh)
+
+            job_stdout = outdirname + '/FARM/outputs/' + dtag + '_' + str(i) + '.cout'
+            dset_bsubs.append(job_command_template.format(job_name = job_name, job_stdout = job_stdout, jobsh = job_sh_filename))
+
+        print("Created {} jobs. Ready to submit.".format(len(dset_bsubs)))
+        with open(outdirname + '/FARM/inputs/' + dtag + '_' + 'bsub.sh') as bsub_file:
+            bsub_file.write('\n'.join(dset_bsubs))
+            print("Wrote the bsub commands to {}".format(bsub_file.name))
+
+        print("Ready to submit.")
+        print("No submit in the test run.")
+
+
+
+
+
+
+
+
