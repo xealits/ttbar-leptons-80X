0f9079f03925ffa9c0b525e0e15a35f0dd39972f
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2016-07-21 20:51:52 +0100

    Fixed things up and added CERN local tier fileserver to the job script

--- job_submit.py
+++ job_submit.py
@@ -98,15 +98,26 @@ with open(cfg_templ_filename) as t_f, open(dsets_json_filename) as d_f:
     dsets = json.load(d_f)
     cfg_templ = t_f.read()
 
+
 job_dir = outdirname + "/FARM/inputs/"
 job_outs = outdirname + "/FARM/outputs/"
 
 
 # Make job FARM directories
 
-os.makedirs(job_dir)
-os.makedirs(job_outs)
-project_dir = os.getcwd()
+print("Making the task (job batch) area in {}".format(outdirname))
+
+try:
+    os.makedirs(job_dir)
+    os.makedirs(job_outs)
+except OSError, e:
+    if e.errno == 17:
+        print("The directories exist, no need to make them.")
+    else:
+        raise
+
+project_dir = os.getcwd() + '/' # just in case
+print("Project dir  is set to:", project_dir)
 
 # Job templates
 
@@ -131,9 +142,30 @@ def chunks(l, n):
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
 
+# Create proxy for working with LSF jobs
 # Go through dsets.json
 # Create jobs.sh
 # Submit them
+
+proxy_file = outdirname + "/FARM/inputs/x509_proxy"
+print("Initializing proxy in " + proxy_file)
+os.system('export X509_USER_PROXY=' + proxy_file + '; voms-proxy-init --voms cms')
+#status, output = commands.getstatusoutput('export X509_USER_PROXY=' + proxy_file + '; voms-proxy-init --voms cms') # instead of voms-proxy-init --voms cms --noregen
+#if status !=0:
+#    print("Proxy initialization failed:")
+#    print("    status = " + str(status))
+#    print("    output =\n" + output)
+#    exit(2)
+
+print("Proxy is ready.")
+
+
+# finding local computing resourses
+hostname = commands.getstatusoutput("hostname -f")[1]
+if "cern.ch" in hostname: local_tier = "T2_CH_CERN"
+
+# default file server is the global server:
+file_server = "root://cms-xrd-global.cern.ch/"
 
 n_files_per_job = 5
 
@@ -141,25 +173,47 @@ for dset_group in dsets['proc']:
     isdata = dset_group.get('isdata', False)
     # TODO: other parameters?
         # two required parameters of a dset
-    for dset in dset_group['data']:
-        dset = dset['dset']
-        dtag = dset['dtag']
-        lumiMask = dset.get('lumiMask', '')
+    for d in dset_group['data']:
+        dtag = d['dtag']
+        dset = d['dset']
+        print("Submitting dtag " + dtag)
+        print(dset)
+
+        lumiMask = d.get('lumiMask', '')
         # TODO: other parameters as well?
 
         # get files of the dset
         status, out = commands.getstatusoutput('das_client.py --limit=0 --query="file dataset={}"'.format(dset))
-        out_rows = out.split('\n')
+        #out_rows = out.split('\n')
+        dset_files = out.split('\n')
 
-        if status != 0 or len(out_rows) < 4:
-            print("Failed fetch files of {} dataset".format(dset))
+        if status != 0 or dset_files < 1:
+            print("Failed fetch _files_ of {} dataset".format(dset))
             print("das_client.py status = " + str(status))
             print("              output = " + out)
             print("Continue to other dsets")
             continue
 
-        dset_files = out_rows[3:]
+        #dset_files = out_rows[3:]
         print("Found {} files. Splitting {} per job.".format(len(dset_files), n_files_per_job))
+
+        status, out = commands.getstatusoutput('das_client.py --query="site dataset={} | grep site.name,site.dataset_fraction" '.format(dset))
+        sites = out.split('\n')
+        if status != 0 or len(sites) < 1:
+            print("Failed to fetch _sites_ of {} dataset".format(dset))
+            print("das_client.py status = " + str(status))
+            print("              output = " + out)
+            print("Continue to other dsets")
+            continue
+
+        if any(local_tier in s and "100.00%" in s for s in sites):
+            print("The full dataset is found on local tier " + local_tier)
+            file_server = "root://eoscms//eos/cms/"
+            print("using " + file_server + " fileserver")
+        else:
+            print("using default " + file_server + " fileserver")
+
+        #sites = sites_rows[3:]
 
         # the list of bsubs job submition commands
         dset_bsubs = []
@@ -168,7 +222,14 @@ for dset_group in dsets['proc']:
         for i, job_chunk in enumerate(chunks(dset_files, n_files_per_job)):
             job_name = exec_name + '_' + dtag + '_' + str(i) + '_'
 
-            job_cfg = cfg_templ.format(input = job_chunk, lumiCert = lumiMask, dtag = dtag, outdir = outdirname, jobID = i, isMC = not isdata, outfile = outdirname + '_' + str(i) + '.root')
+            # if the site of dataset is CERN -- can run directly on from eos
+            #root://eoscms//eos/cms/
+            # finding "physical" files via root://cms-xrd-global.cern.ch/
+            input_files = ',\n'.join('"%s/%s"' % (file_server, s) for s in job_chunk)
+
+            #if i < 5: print(input_files)
+
+            job_cfg = cfg_templ.format(input = input_files, lumiMask = lumiMask, dtag = dtag, outdir = outdirname, job_num = i, isMC = not isdata, outfile = outdirname + dtag + '_' + str(i) + '.root', project_dir = project_dir)
             # job_cfg = cfg_templ.format(job_files = job_chunk, lumiCert = lumiMask, dtag = dtag, outdir = outdirname, jobID = i, isdata = isdata)
 
             job_cfg_filename = outdirname + dtag + '_' + str(i) + '_cfg.py'
@@ -179,17 +240,23 @@ for dset_group in dsets['proc']:
             job_sh_filename = outdirname + '/FARM/inputs/' + dtag + '_' + str(i) + '.sh'
             with open(job_sh_filename, 'w') as sh_file:
                 sh_file.write(job_sh)
+                os.system("chmod 777 " + sh_file.name)
 
             job_stdout = outdirname + '/FARM/outputs/' + dtag + '_' + str(i) + '.cout'
             dset_bsubs.append(job_command_template.format(job_name = job_name, job_stdout = job_stdout, jobsh = job_sh_filename))
 
         print("Created {} jobs. Ready to submit.".format(len(dset_bsubs)))
-        with open(outdirname + '/FARM/inputs/' + dtag + '_' + 'bsub.sh') as bsub_file:
+        with open(outdirname + '/FARM/inputs/' + dtag + '_' + 'bsub.sh', 'w') as bsub_file:
             bsub_file.write('\n'.join(dset_bsubs))
             print("Wrote the bsub commands to {}".format(bsub_file.name))
 
         print("Ready to submit.")
-        print("No submit in the test run.")
+        #print("No submit in the test run.")
+        #print("printing the bsubs instead")
+
+        for bsub in dset_bsubs:
+            #print(bsub)
+            print(commands.getstatusoutput(bsub))
 
 
 
