25d8a7212705950dcd354b95bfceefaec1032c35
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2016-08-16 18:48:42 +0200

    Jet-to-tau fakerate: correct TH3F to csv procedure

--- csv_TH3Fs.cc
+++ csv_TH3Fs.cc
@@ -21,23 +21,50 @@ int csv_TH3Fs(TString filename1, TString filename2, TString out_filename) {
 	h1->Print();
 	h2->Print();
 
-	fprintf(out_f, "i_bin,x,y,z,tau_jets_bin,jets_bin");
+	fprintf(out_f, "i_bin,x,y,z,tau_jets_bin,jets_bin\n");
 
-	for (int i=0; h1->GetSize(); ++i)
+	Int_t size = h1->GetSize();
+	bool passed_midwork = 0;
+	cout << "the histos size = " << size << " and " << h2->GetSize() << "\n";
+
+	Int_t size_x = h1->GetNbinsX();
+	Int_t size_y = h1->GetNbinsY();
+	Int_t size_z = h1->GetNbinsZ();
+	cout << "the histos size = " << size_x << " , " << size_y << " , " << size_z << "\n";
+	for (int iz=0; iz<size_z; ++iz)
+	{
+	for (int ix=0; ix<size_x; ++ix)
+	{
+	for (int iy=0; iy<size_y; ++iy)
 		{
-		double xcenter1 = h1->GetZaxis()->GetBinCenter(i);
-		double xcenter2 = h2->GetZaxis()->GetBinCenter(i);
-		double ycenter1 = h1->GetZaxis()->GetBinCenter(i);
-		double ycenter2 = h2->GetZaxis()->GetBinCenter(i);
-		double zcenter1 = h1->GetZaxis()->GetBinCenter(i);
-		double zcenter2 = h2->GetZaxis()->GetBinCenter(i);
+		/*
+		if ((i / size) > 0.5 && ! passed_midwork)
+			{
+			passed_midwork = 1;
+			cout << "Passed midwork!\n";
+			}
+		*/
+		double xcenter1 = (double) h1->GetXaxis()->GetBinCenter(ix);
+		double xcenter2 = (double) h2->GetXaxis()->GetBinCenter(ix);
+		double ycenter1 = (double) h1->GetYaxis()->GetBinCenter(iy);
+		double ycenter2 = (double) h2->GetYaxis()->GetBinCenter(iy);
+		double zcenter1 = (double) h1->GetZaxis()->GetBinCenter(iz);
+		double zcenter2 = (double) h2->GetZaxis()->GetBinCenter(iz);
 		if (xcenter1 != xcenter2) return 1;
 
-		double n_jets = h1->GetBinContent(i);
-		double n_tau_jets = h2->GetBinContent(i);
-		//fprintf(out_f, "%d,%g,%g,%g,%g,%g\n", i, xcenter1, ycenter1, zcenter1, n_jets, n_tau_jets);
-		fprintf(out_f, "%d,%g,%g\n", i, n_jets, n_tau_jets);
+		// some brilliance from ROOT histograms (f*cking 3d array!)
+		// Int_t bin = h->GetBin(binx,biny,binz);
+		Int_t global_bin = h1->GetBin(ix,iy,iz);
+		Int_t global_bin2 = h2->GetBin(ix,iy,iz);
+		if (global_bin != global_bin2) return 2;
+
+		double n_jets     = h1->GetBinContent( global_bin );
+		double n_tau_jets = h2->GetBinContent( global_bin );
+		fprintf(out_f, "%d,%g,%g,%g,%g,%g\n", global_bin, xcenter1, ycenter1, zcenter1, n_jets, n_tau_jets);
+		//fprintf(out_f, "%d,%g,%g\n", i, n_jets, n_tau_jets);
 		}
+	}
+	}
 
 	file1->Close();
 	file2->Close();
