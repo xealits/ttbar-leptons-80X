2b2fed1e3e6cc210bf963e2ff3a7d129094cca30
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2016-11-03 06:02:13 +0100

    Jobbing: almost got the stacked histogram, it needs hadd-like utility for xsec-lumi normalized nicknamed MC

--- jobing/stacked_histo_distr.cc
+++ jobing/stacked_histo_distr.cc
@@ -4,6 +4,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TCanvas.h"
+#include "THStack.h"
 #include "TH1F.h"                                                                                                                                       
 #include "TH2F.h"
 #include "TH3F.h"
@@ -94,46 +95,98 @@ std::map<TString, double> xsecs = {
 {"MC2016_noHLT_tchannel_top_4f_leptonicDecays_powheg.root", 70.69/2}
 };
 
+
+/*
+std::map<TString, EColor> dtag_colours = {
+{"Data13TeV_SingleElectron2016D_PromptRecoV2_", kAzure},
+{"Data13TeV_SingleMuon2016D_PromptRecoV2_",     kAzure},
+{"MC2016_noHLT_DYJetsToLL_10to50_amcatnlo.root",  kGray},
+{"MC2016_noHLT_DYJetsToLL_50toInf_amcatnlo.root", kGray},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_elelbar"         , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_elmubar"         , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_elqbar"          , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_eltaubar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_muelbar"         , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_mumubar"         , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_muqbar"          , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_mutaubar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_qelbar"          , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_qmubar"          , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_qqbar"           , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_qtaubar"         , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_tauelbar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_taumubar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_tauqbar"         , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaleup_tautaubar"       , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_elelbar"       , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_elmubar"       , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_elqbar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_eltaubar"      , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_muelbar"       , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_mumubar"       , (kGreen-3)},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_muqbar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_mutaubar"      , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_qelbar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_qmubar"        , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_qqbar"         , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_qtaubar"       , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_tauelbar"      , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_taumubar"      , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_tauqbar"       , kGreen},
+{ "MC2016_noHLT_TTJets_powheg_scaledown_tautaubar"     , kGreen},
+{"MC2016_noHLT_W0Jets_amcatnlo.root", kOrange},
+{"MC2016_noHLT_W1Jets_madgraph.root", kOrange},
+{"MC2016_noHLT_W2Jets_madgraph.root", kOrange},
+{"MC2016_noHLT_W3Jets_madgrapg.root", kOrange},
+{"MC2016_noHLT_W4Jets_madgraph.root", kOrange},
+{"MC2016_noHLT_WW.root", kCyan},
+{"MC2016_noHLT_WZ.root", kCyan},
+{"MC2016_noHLT_ZZ.root", kCyan},
+{"MC2016_noHLT_SingleT_tW_5FS_powheg.root", kAzure},
+{"MC2016_noHLT_SingleTbar_tW_5FS_powheg.root", kAzure},
+{"MC2016_noHLT_schannel_4FS_leptonicDecays_amcatnlo.root", kAzure},
+{"MC2016_noHLT_tchannel_antitop_4f_leptonicDecays_powheg.root", kAzure},
+{"MC2016_noHLT_tchannel_top_4f_leptonicDecays_powheg.root", kAzure}
+};
+*/
+
+Color_t dtag_colours(TString dtag)
+	{
+	if (dtag.Contains("Data")) return kWhite;
+	else if(dtag.Contains("DYJets")) return kGray;
+	else if(dtag.Contains("W0Jets") ||dtag.Contains("W4Jets") ||dtag.Contains("W1Jets") ||dtag.Contains("W2Jets") ||dtag.Contains("W3Jets") ||dtag.Contains("WJets") ) return kOrange;
+	else if(dtag.Contains("WW") ||dtag.Contains("WZ") ||dtag.Contains("ZZ")) return kCyan;
+	else if(dtag.Contains("Single") || dtag.Contains("schannel") ||dtag.Contains("tchannel")) return kAzure;
+	else if(dtag.Contains("TT"))
+		{
+		if (dtag.Contains("qqbar")) return kGreen+4;
+		else if (dtag.Contains("elqbar") || dtag.Contains("qelbar") ||dtag.Contains("muqbar") || dtag.Contains("qmubar") || dtag.Contains("tauqbar") || dtag.Contains("qtaubar")) return kGreen+3;
+		else return kGreen-10;
+		}
+	else return kBlack;
+
+	}
+
 //int stacked_histo_distr (int argc, char *argv[])
 int main (int argc, char *argv[])
 {
-if (argc < 4)
+if (argc < 5)
 	{
-	std::cout << "Usage : " << argv[0] << " distr, dir, dtags" << std::endl;
+	std::cout << "Usage : " << argv[0] << " lumi, distr, dir, dtags" << std::endl;
 	exit (0);
 	}
 
 gROOT->Reset();
 
-TString distr(argv[1]);
-TString dir(argv[2]);
-TString dtag1(argv[3]);
+double lumi = atof(argv[1]);
+TString distr(argv[2]);
+TString dir(argv[3]);
+TString dtag1(argv[4]);
+
+cout << lumi  << endl;
 cout << distr << endl;
 cout << dir   << endl;
 cout << dtag1 << endl;
-
-
-std::vector < TString > dtags;
-std::vector < TFile * > files;
-std::vector < TH1D * > histos;
-//vector<int> dtags;
-//dtags.reserve();
-
-for (int i = 3; i<argc; i++)
-	{
-	TString dtag(argv[i]);
-	dtags.push_back(dtag);
-	files.push_back(TFile::Open(dir + "/" + dtag + ".root"));
-	
-	//dtags.push_back(5);
-	}
-
-for (int i = 0; i<dtags.size(); i++)
-	{
-	cout << dtags[i] << endl;
-	histos.push_back((TH1D*) files[i]->Get(distr));
-	histos[i]->Print();
-	}
 
 for(std::map<TString, double>::iterator it = xsecs.begin(); it != xsecs.end(); ++it)
 	{
@@ -141,6 +194,73 @@ for(std::map<TString, double>::iterator it = xsecs.begin(); it != xsecs.end(); +
 	double xsec  = it->second;
 	cout << "For dtag " << dtag << " xsec " << xsec << "\n";
 	}
+
+
+std::vector < TString > dtags;
+std::vector < TFile * > files;
+std::vector < TH1D * > histos;
+std::vector < TH1D * > weightflows;
+//vector<int> dtags;
+//dtags.reserve();
+
+// make stack of MC, scaling according to ratio = lumi * xsec / weightflow4 (bin5?)
+// also nickname the MC....
+// per-dtag for now..
+
+//THStack *hs = new THStack("hs","Stacked 1D histograms");
+THStack *hs      = new THStack("hs", "");
+THStack *hs_data = new THStack("hs", "");
+
+for (int i = 4; i<argc; i++)
+	{
+	TString dtag(argv[i]);
+	cout << "processing " << dtag << endl;
+	dtags.push_back(dtag);
+	files.push_back(TFile::Open(dir + "/" + dtag + ".root"));
+	//dtags.push_back(5);
+
+	if (!files.back()->GetListOfKeys()->Contains(distr))
+		{
+		cout << "no " << distr << endl;
+		continue;
+		}
+
+	weightflows.push_back((TH1D*) files.back()->Get("weightflow_el"));
+	weightflows.back()->Print();
+
+	histos.push_back((TH1D*) files.back()->Get(distr));
+	histos.back()->Print();
+	if (dtags.back().Contains("Data"))
+		{
+		cout << "summing data-stack" << endl;
+		histos.back()->SetMarkerStyle(9);
+		//histos.back()->SetFillColor(kRed + i);
+		hs_data->Add(histos.back());
+		}
+	else
+		{
+		cout << "scaling and adding a stack histo" << endl;
+		histos.back()->Scale(lumi * xsecs[dtags.back()] / weightflows.back()->GetBinContent(5));
+		histos.back()->Print();
+		//histos.back()->SetFillColor(kRed );
+		histos.back()->SetFillColor( dtag_colours(dtag) );
+		histos.back()->SetMarkerStyle(20);
+		histos.back()->SetLineStyle(0);
+		histos.back()->SetMarkerColor(i);
+		hs->Add(histos.back(), "HIST");
+		}
+
+	}
+
+TCanvas *cst = new TCanvas("cst","stacked hists",10,10,700,700);
+//cst->SetFillColor(41);
+cst->Divide(1,1);
+// in top left pad, draw the stack with defaults
+cst->cd(1);
+hs->Draw();
+hs_data->Draw("e p same");
+
+cst->SaveAs("./teststack.png");
 
 /*
 TH1D *h = (TH1D*) file->Get(distr);
