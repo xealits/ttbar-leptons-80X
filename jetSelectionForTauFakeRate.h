3e889cad658f17a276b09c3dc82f1fe929b1de1a
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2016-12-21 14:50:03 +0100

    Jet fake rates: fix in record of pdgId jet origin

--- jetSelectionForTauFakeRate.h
+++ jetSelectionForTauFakeRate.h
@@ -149,7 +149,7 @@ int record_jets_fakerate_distrs(string channel, string selection, pat::JetCollec
 			//qcd_jet_origin->Fill(abs( jet.partonFlavour() ));
 			fill_1d(channel + selection + ("_jet_partonFlavour"), 100, 0, 100,   abs(jet.partonFlavour()), event_weight);
 			fill_1d(channel + selection + ("_jet_hadronFlavour"), 100, 0, 100,   abs(jet.hadronFlavour()), event_weight);
-			fill_1d(channel + selection + ("_jet_genParton_pdgId"), 100, 0, 100,   abs(jet.genParton().pdgId()), event_weight);
+			fill_1d(channel + selection + ("_jet_genParton_pdgId"), 100, 0, 100,   abs(jet.genParton()->pdgId()), event_weight);
 			//qcd_jet_origin->Fill(abs( jet_origin->pdgId() ));
 			// qcd_taujet_origin
 			}
