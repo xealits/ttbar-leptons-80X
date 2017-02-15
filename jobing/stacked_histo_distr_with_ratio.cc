a281ec0b45ead89306a15c41121fcb747f48f221
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2017-02-15 20:39:08 +0100

    Jobing: a working stacket+ratio plotter

--- jobing/stacked_histo_distr_with_ratio.cc
+++ jobing/stacked_histo_distr_with_ratio.cc
@@ -16,6 +16,8 @@
 #include "TNtuple.h"
 #include <Math/VectorUtil.h>
 
+#include "TStyle.h"
+
 #include <map>
 #include <string>
 #include <vector>
@@ -140,10 +142,13 @@ for (int i = DTAG_ARGS_START; i<argc; i++)
 			//nicknamed_mc_histos[nick] = new TH1D(nick, "");
 		else
 			nicknamed_mc_histos[nick]->Add(histos.back());
+
 		//hs->Add(histos.back(), "HIST");
 		}
 	}
 
+TH1D    *hs_sum  = NULL;
+// build the stack of MC histos
 //std::map<TString, TH1D *> nicknamed_mc_histos;
 for(std::map<TString, TH1D*>::iterator it = nicknamed_mc_histos.begin(); it != nicknamed_mc_histos.end(); ++it)
 	{
@@ -158,6 +163,15 @@ for(std::map<TString, TH1D*>::iterator it = nicknamed_mc_histos.begin(); it != n
 	//entry=leg->AddEntry("singlemu_altstep4rho3","Single top","F");
 	leg->AddEntry(distr, nick, "F");
 
+	if (hs_sum == NULL)
+		{
+		hs_sum = (TH1D*) (distr->Clone("brand_new_hs_sum"));
+		hs_sum->SetName("hs_sum");
+		hs_sum->ResetBit(kCanDelete);
+		}
+	else
+		hs_sum->Add(distr);
+
 	//distr->SetName(controlpoint_name.c_str());
 	//distr->Write();
 	//out_f->Write(controlpoint_name.c_str());
@@ -168,6 +182,9 @@ for(std::map<TString, TH1D*>::iterator it = nicknamed_mc_histos.begin(); it != n
 //cst->Divide(1,1);
 // in top left pad, draw the stack with defaults
 //cst->cd(1);
+
+TStyle* m_gStyle = new  TStyle();
+m_gStyle->SetOptStat(111100);
 
 cout << "setting title" << endl;
 
@@ -187,26 +204,116 @@ while ((obj=next())) {
 */
 
 
-TH1F * h = cst->DrawFrame(0.,0.,1.,1.);
-h->SetXTitle("x");
+//TH1F * h = cst->DrawFrame(0.,0.,1.,1.);
+//h->SetXTitle("x");
 //cst->Update();
 //cst->Modified();
 
 cout << "drawing" << endl;
 
-hs->Draw();
-hs_data->Draw("e p same");
+TPad *pad1 = new TPad("pad1","This is pad1", 0., 0.2, 1., 1.);
+TPad *pad2 = new TPad("pad2","This is pad2", 0., 0.,  1., 0.2);
+//pad1->SetFillColor(11);
+//pad2->SetFillColor(11);
+pad1->Draw();
+pad2->Draw();
 
+pad1->cd();
+
+hs_data->GetXaxis()->SetLabelFont(63);
+hs_data->GetXaxis()->SetLabelSize(14); // labels will be 14 pixels
+hs_data->GetYaxis()->SetLabelFont(63);
+hs_data->GetYaxis()->SetLabelSize(14); // labels will be 14 pixels
+
+hs->Draw("UO"); // the stack
+// also draw the sum of the stack and its' errors:
+//((TObjArray*) hs->GetStack())->Last()->Draw("e4 same"); // then, how to set styles with the hatched error bars?
+
+if (hs_sum != NULL)
+	{
+	hs_sum->Print();
+	hs_sum->SetFillStyle(3004);
+	hs_sum->SetFillColor(1);
+	hs_sum->SetMarkerColorAlpha(0, 0.1);
+	hs_sum->SetMarkerStyle(1);
+	hs_sum->SetMarkerColor(0);
+	hs_sum->Draw("UO e2 same"); // the errors on the stack
+	}
+else
+	cout << "NO HS_SUM!!!!!!!!!!!!!!" << endl;
+
+hs_data->Draw("UO e p same"); // the data with the errors
+
+// histo->SetTitle("boxtitle;x axis title [unit];y axis title [unit]")
+cout << "setting the stack title" << endl;
+hs->GetXaxis()->SetTitle(distr);
+cout << "done setting the stack title" << endl;
+hs_data->SetXTitle(distr);
+hs_sum->SetXTitle(distr);
+
+leg->SetBorderSize(0);
 leg->Draw();
 
 //MC_stack_124->GetXaxis()->SetTitle("#rho");
 //mcrelunc929->GetYaxis()->SetTitle("Data/#Sigma MC");
 
 
-hs->GetXaxis()->SetTitle(distr);
-hs_data->SetXTitle(distr);
 
-cst->SetLogy();
+//cst->SetLogy();
+//cst->Modified();
+
+pad2->cd();
+
+TH1D * hs_data_relative = (TH1D*) hs_data->Clone();
+TH1D * hs_sum_relative  = (TH1D*) hs_sum->Clone();
+
+hs_data_relative->SetXTitle("");
+hs_sum_relative->SetXTitle("");
+
+hs_data_relative->SetMarkerColor(1);
+
+// to have the same font size on both pads:
+hs_data_relative->GetXaxis()->SetLabelFont(63);
+hs_data_relative->GetXaxis()->SetLabelSize(14); // labels will be 14 pixels
+hs_data_relative->GetYaxis()->SetLabelFont(63);
+hs_data_relative->GetYaxis()->SetLabelSize(14); // labels will be 14 pixels
+hs_sum_relative->GetXaxis()->SetLabelFont(63);
+hs_sum_relative->GetXaxis()->SetLabelSize(14); // labels will be 14 pixels
+hs_sum_relative->GetYaxis()->SetLabelFont(63);
+hs_sum_relative->GetYaxis()->SetLabelSize(14); // labels will be 14 pixels
+
+
+for (int i=0; i<=hs_sum_relative->GetSize(); i++)
+	{
+	double mc_content = hs_sum_relative->GetBinContent(i);
+	double mc_error   = hs_sum_relative->GetBinError(i);
+	//double width   = histo->GetXaxis()->GetBinUpEdge(i) - histo->GetXaxis()->GetBinLowEdge(i);
+	hs_sum_relative->SetBinContent(i, 1);	
+	hs_sum_relative->SetBinError(i, mc_error/mc_content);	
+
+	double data_content = hs_data_relative->GetBinContent(i);
+	double data_error   = hs_data_relative->GetBinError(i);
+	//double width   = histo->GetXaxis()->GetBinUpEdge(i) - histo->GetXaxis()->GetBinLowEdge(i);
+	if (mc_content > 0)
+		{
+		hs_data_relative->SetBinContent(i, data_content/mc_content);
+		hs_data_relative->SetBinError(i, data_error/mc_content);
+		}
+	else
+		{
+		hs_data_relative->SetBinContent(i, 1);
+		hs_data_relative->SetBinError(i, data_error);
+		}
+	}
+
+hs_sum_relative->SetStats(false);
+hs_data_relative->SetStats(false);
+hs_sum_relative->GetYaxis()->SetRange(0.5, 1.5);
+hs_data_relative->GetYaxis()->SetRangeUser(0.5, 1.5);
+
+hs_sum_relative->Draw("e2");
+hs_data_relative->Draw("e p same");
+
 cst->Modified();
 
 cst->SaveAs( dir + "/jobsums/" + distr + ".png" );
