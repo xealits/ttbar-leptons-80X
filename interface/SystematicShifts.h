a8cb0549950a51afcaddca58a76b14d96c64fb4a
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2017-04-03 19:26:19 +0100

    Project: forgot the systematic header..

---
+++ interface/SystematicShifts.h
@@ -0,0 +1,19 @@
+#include <map>
+using namespace std;
+
+
+typedef enum {SYS_NOMINAL, SYS_PU_UP, SYS_PU_DOWN} systematic_shift;
+
+systematic_shift allSystematics[] = {SYS_NOMINAL, SYS_PU_UP, SYS_PU_DOWN};
+
+// C++11 feature:
+map<systematic_shift, const char*> systematic_shift_names = {{SYS_NOMINAL, "NOMINAL"}, {SYS_PU_UP, "PU_DOWN"}, {SYS_PU_DOWN, "PU_DOWN"}};
+
+/*
+const char * systematic_shift_name[] = {
+    "NOMINAL",
+    "PU_UP",
+    "PU_DOWN",
+};
+*/
+
