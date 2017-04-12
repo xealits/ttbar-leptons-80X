26fdca09527edf01ee2e9f9c75b7e1366e43ce0b
Alex Toldaiev <oleksii.toldaiev@gmail.com>
2017-04-12 18:13:24 +0100

    Fix in systematics names: PU_UP

--- interface/SystematicShifts.h
+++ interface/SystematicShifts.h
@@ -7,7 +7,7 @@ typedef enum {SYS_NOMINAL, SYS_PU_UP, SYS_PU_DOWN} systematic_shift;
 systematic_shift allSystematics[] = {SYS_NOMINAL, SYS_PU_UP, SYS_PU_DOWN};
 
 // C++11 feature:
-map<systematic_shift, const char*> systematic_shift_names = {{SYS_NOMINAL, "NOMINAL"}, {SYS_PU_UP, "PU_DOWN"}, {SYS_PU_DOWN, "PU_DOWN"}};
+map<systematic_shift, const char*> systematic_shift_names = {{SYS_NOMINAL, "NOMINAL"}, {SYS_PU_UP, "PU_UP"}, {SYS_PU_DOWN, "PU_DOWN"}};
 
 /*
 const char * systematic_shift_name[] = {
