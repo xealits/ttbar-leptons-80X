771b69b3056931930484c4b3ae4a384e3fb94c5c
Oleksii Toldaiev <alex.toldaiev@cern.ch>
2017-05-10 18:45:11 +0200

    elmu fake rates: 1D/2D distrs fir loose, medium and tight taus (vtight, vloose to add)

--- src/recordFuncs.cc
+++ src/recordFuncs.cc
@@ -246,6 +246,21 @@ static int n_bins_rad = 35;
 
 
 
+
+// larger bins for 1D/2D
+static Float_t rebins_pt[23] = { 0,    20, 21, 23,    25, 27, 30, 33, 36, 39,    42, 46, 50, 55, 60, 65, 70, 80, 90, 100, 125, 150, 200 }; // 22 rebins 23 edges
+static int n_rebins_pt = 22;
+
+static Float_t rebins_eta[28] = { -3.0,    -2.5, -2.3, -2.1, -1.9, -1.7, -1.5, -1.3, -1.1, -0.9, -0.7, -0.5, -0.3, -0.1, 0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 1.5, 1.7, 1.9, 2.1, 2.3, 2.5,    3.0 }; // 27 rebins 28 edges
+static int n_rebins_eta = 27;
+
+// 0.1 radius rebins:
+static Float_t rebins_rad[34] = { 0, 0.012,   0.025, 0.032, 0.04,   0.05, 0.06,   0.075, 0.082, 0.09,   0.1, 0.112,   0.125, 0.132, 0.14,   0.15, 0.16,   0.175, 0.182, 0.19,   0.2, 0.21, 0.22, 0.23, 0.24,    0.25, 0.26, 0.27, 0.28, 0.29,    0.3, 0.31, 0.32,    0.4 }; // 33 rebins 34 edges
+static int n_rebins_rad = 33;
+
+
+
+
 double jet_radius(pat::Jet& jet)
 	{
 	//return sqrt(jet.EtaPhiMoments::etaEtaMoment + jet.EtaPhiMoments::phiPhiMoment);
@@ -260,6 +275,67 @@ double jet_radius(pat::Tau& jet)
 	return sqrt(jet.etaetaMoment() + jet.phiphiMoment());
 	}
 
+
+int fill_jet_distrs_1D_2D(string control_point_name, Double_t weight, Double_t pt, Double_t eta, Double_t radius)
+	{
+	// these "fill jets" functions basically handle the non-linear binning
+
+	// for given (mc_decay, control_point) record the distributions of the given jet parameters in histograms of given pt and eta bins
+	// here I record their 1D pt, eta, radius and 2D pt-eta histograms
+	//
+	// first check if the given control_point histograms were created (check only 2D distrs, assume 1D)
+	// create new histograms if needed
+	// check if the key (mc_decay, control point) has been initialized
+	// std::pair <string,string> key (mc_decay, control_point_name);
+
+	// check mc_decay in the maps
+	// create channel map in th3d_distr_maps_control
+	if (th2d_distr_maps_control.find(mc_decay) == th2d_distr_maps_control.end() )
+		th2d_distr_maps_control.insert( std::make_pair(mc_decay, std::map<string, TH2D>()));
+	if (th1d_distr_maps_control.find(mc_decay) == th1d_distr_maps_control.end() )
+		th1d_distr_maps_control.insert( std::make_pair(mc_decay, std::map<string, TH1D>()));
+
+	// if the control_point is not found for the mc_decay, create it
+	// (in 2D and 1D histograms)
+	string pt_eta_name = control_point_name + string("_pt_eta");
+	string pt_name  = control_point_name + string("_pt");
+	string eta_name = control_point_name + string("_eta");
+	string radius_name = control_point_name + string("_radius");
+	if (th2d_distr_maps_control[mc_decay].find(pt_eta_name) == th2d_distr_maps_control[mc_decay].end() )
+		{
+		// the control point distr has not been created/initialized
+		// THE HISTOGRAM NAMES HAVE TO BE DIFFERENT
+		th2d_distr_maps_control[mc_decay][pt_eta_name] = TH2D((pt_eta_name + mc_decay).c_str(), ";;", n_rebins_pt, rebins_pt, n_rebins_eta, rebins_eta);
+		th1d_distr_maps_control[mc_decay][pt_name]     = TH1D((pt_name  + mc_decay).c_str(), ";;", n_rebins_pt, rebins_pt);
+		th1d_distr_maps_control[mc_decay][eta_name]    = TH1D((eta_name + mc_decay).c_str(), ";;", n_rebins_eta, rebins_eta);
+		th1d_distr_maps_control[mc_decay][radius_name] = TH1D((radius_name + mc_decay).c_str(), ";;", n_rebins_rad, rebins_rad);
+		// later on, when writing to the file,
+		// I'll have to rename histograms on each write
+		// and probably delete them along the way, so that they don't collide...
+		// ROOT SUCKS
+		//cout << "creating " << control_point_name << endl;
+		}
+
+	// fill the distribution:
+	//th3d_distr_maps_control[mc_decay][control_point_name].Fill(pt, eta, radius, weight);
+	th2d_distr_maps_control[mc_decay][pt_eta_name].Fill(pt, eta);
+	th1d_distr_maps_control[mc_decay][pt_name]    .Fill(pt);
+	th1d_distr_maps_control[mc_decay][eta_name]   .Fill(eta);
+	th1d_distr_maps_control[mc_decay][radius_name].Fill(radius);
+
+	// not sure where header-histograms are used and why
+	if (th2d_distr_maps_control_headers.find(pt_eta_name) == th2d_distr_maps_control_headers.end() )
+		th2d_distr_maps_control_headers.insert( std::make_pair(pt_eta_name, TH2D((string("Header of ") + pt_eta_name).c_str(), ";;", n_rebins_pt, rebins_pt, n_rebins_eta, rebins_eta)));
+	if (th1d_distr_maps_control_headers.find(pt_name) == th1d_distr_maps_control_headers.end() )
+		th1d_distr_maps_control_headers.insert( std::make_pair(pt_name, TH1D((string("Header of ") + pt_name).c_str(), ";;", n_rebins_pt, rebins_pt)));
+	if (th1d_distr_maps_control_headers.find(eta_name) == th1d_distr_maps_control_headers.end() )
+		th1d_distr_maps_control_headers.insert( std::make_pair(eta_name, TH1D((string("Header of ") + eta_name).c_str(), ";;", n_rebins_eta, rebins_eta)));
+	if (th1d_distr_maps_control_headers.find(radius_name) == th1d_distr_maps_control_headers.end() )
+		th1d_distr_maps_control_headers.insert( std::make_pair(radius_name, TH1D((string("Header of ") + radius_name).c_str(), ";;", n_rebins_rad, rebins_rad)));
+
+	// return success:
+	return 0;
+	}
 
 
 int fill_jet_distr(string control_point_name, Double_t weight, Double_t pt, Double_t eta, Double_t radius)
@@ -399,6 +475,145 @@ int fill_btag_efficiency(string control_point_name, Double_t pt, Double_t eta, D
 	return 0;
 	}
 
+
+
+
+int record_jets_fakerate_distrs_1D_2D(string channel, string selection, pat::JetCollection & selJets, pat::TauCollection & selTaus, vector<LorentzVector>& visible_gen_taus, double event_weight, bool isMC)
+	{
+	// for control record distr of given taus (not the matching jets -- just taus)
+	for (size_t i = 0; i < selTaus.size(); ++i)
+		{
+		pat::Tau & tau = selTaus[i];
+		// Calo Taus don't have radius?
+		fill_2d(channel + selection + ("_taus_distr"), 100, 0., 300., 50, -2.5, 2.5, tau.pt(), tau.eta(), event_weight);
+		}
+
+	for (size_t ijet = 0; ijet < selJets.size(); ++ijet)
+		{
+		pat::Jet& jet = selJets[ijet];
+		// qcd_jets_distr->Fill(jet.pt(), jet.eta(), jet_radius(jet));
+		// THE DISTRIBUTION OF PROBE JETS, all probe jets in the event
+		fill_jet_distrs_1D_2D(channel + selection + ("_jets_distr"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+		//fill_3d(channel + selection + ("_jets_distr"), 10, bins_pt, n_bins_eta, bins_eta, 15, bins_rad, 300, 0, 300,   20, event_weight);
+
+		// study jet_R ~ jet_eta -- does calculation of R depend on eta of jet?
+		// (maybe it would be nice to use bins of the 3d jet distr?)
+		fill_2d(channel + selection + ("_jet_pt_vs_eta"), 100, 0., 400., 50, -2.5, 2.5, jet.pt(), jet.eta(), event_weight);
+		fill_2d(channel + selection + ("_jet_radius_vs_eta"), 100, 0., 2., 120, -3., 3., jet_radius(jet), jet.eta(), event_weight);
+
+		int jet_origin = -1; // for MC only
+
+		// const reco::GenParticle* genParton()
+		// jet parton origin
+		if (isMC)
+			{
+			//const reco::GenParticle* jet_origin = jet.genParton();
+			// the ID should be in:
+			// jet_origin->pdgId();
+			jet_origin = abs(jet.partonFlavour());
+
+			// match to visible_gen_taus
+			// if partonFlavour == 0 and matches to a tau (dR < tau_fake_distance (~= 0.3))
+			// add it as 15 --- tau
+
+			double minDRtj (9999.);
+			//unsigned int closest_tau = -1; // not used now
+			for (size_t i = 0; i < visible_gen_taus.size(); i++)
+				{
+				double jet_tau_distance = TMath::Min(minDRtj, reco::deltaR (jet, visible_gen_taus[i]));
+				if (jet_tau_distance<minDRtj)
+					{
+					//closest_tau = i;
+					minDRtj = jet_tau_distance;
+					}
+				}
+			if (jet_origin == 0 && (minDRtj < tau_fake_distance)) jet_origin = 15; // BEST so far
+			// maybe the true taus are lost here?
+			// 1) dR can be too wide and include non-tau jets
+			//    (where is the tau jet then? -- maybe mixed into a wide jet, or not reconstructed altogether?) 
+			// 2) what if partonFlavour of a tau jet is not 0?
+			//    maybe 0 are only wide tau jets, which have less ID efficiency,
+			//    average tau jets usually get 1-4 light-quark partonFlavour etc
+			// let's try 2)
+			//if (minDRtj < tau_fake_distance) jet_origin = 15;
+			// the fake rate in qcd is lower than it was and lower than in wjets
+			// try partonFlavour != gluons
+			//if (jet_origin != 21 && (minDRtj < tau_fake_distance)) jet_origin = 15;
+			// 3) TODO: can also try light tau & other + light quarks (partonFlavour < 5)
+			//if (jet_origin < 5 && (minDRtj < tau_fake_distance)) jet_origin = 15;
+
+			fill_1d(channel + selection + ("_jet_partonFlavour"), 30, 0, 30,   jet_origin, event_weight);
+			fill_1d(channel + selection + ("_jet_hadronFlavour"), 30, 0, 30,   abs(jet.hadronFlavour()), event_weight);
+
+			// per-jet-origin distrs
+			if (jet_origin == 21) // gluons
+				fill_jet_distrs_1D_2D(channel + selection + ("_jets_distr_g"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+			else if (jet_origin == 5) // b-quarks
+				fill_jet_distrs_1D_2D(channel + selection + ("_jets_distr_b"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+			else if (jet_origin == 0) // other stuff (taus and PU jets probably)
+				fill_jet_distrs_1D_2D(channel + selection + ("_jets_distr_o"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+			else if (jet_origin == 15) // the matched to gen_taus (dR < 1) jets
+				fill_jet_distrs_1D_2D(channel + selection + ("_jets_distr_t"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+			else // other (light) quarks
+				fill_jet_distrs_1D_2D(channel + selection + ("_jets_distr_q"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+			}
+
+		for(size_t itau=0; itau < selTaus.size(); ++itau)
+			{
+			// selection taus (fake taus)
+			//for (size_t ijet = 0; ijet < selJets.size(); ++ijet)
+			//{
+			//double fake_distance = reco::deltaR(selJets[ijet], selTaus[itau]);
+			double fake_distance = reco::deltaR(jet, selTaus[itau]);
+			//qcd_taujet_distance->Fill(fake_distance);
+			fill_1d(channel + selection + ("_taujet_distance"), 100, 0, 2,   fake_distance, event_weight);
+
+			if (fake_distance <= tau_fake_distance)
+				{
+				// the tau is fake by this jet -- save distr
+				//qcd_tau_jets_distr->Fill(jet.pt(), jet.eta(), jet_radius(jet));
+				// THE DISTRIBUTION OF TAU-JETS, probe-jets dR-matched to taus
+				fill_jet_distrs_1D_2D(channel + selection + ("_tau_jets_distr"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+				// fill_pt_pt(string control_point_name, double pt1, double pt2, double event_weight)
+				//fill_pt_pt(channel + selection + ("_tau_taujet_pts"), jet.pt(), selTaus[itau].pt(), event_weight);
+				fill_2d(channel + selection + ("_tau_taujet_pts"), 400, 0., 400., 400, 0., 400, jet.pt(), selTaus[itau].pt(), event_weight);
+
+				// N tau-jets
+				//increment( channel + selection + ("_selection_ntaujet"), event_weight );
+
+				// const reco::GenParticle* genParton()
+				// jet parton origin for faking jet (just in case)
+				if (isMC)
+					{
+					//int jet_origin = abs(jet.partonFlavour());
+					//const reco::GenParticle* jet_origin = selJets[ijet].genParton();
+					// the ID should be in:
+					// jet_origin->pdgId();
+					//qcd_taujet_origin->Fill(abs( jet.partonFlavour() ));
+					fill_1d(channel + selection + ("_taujet_origins"), 30, 0, 30,   jet_origin, event_weight);
+					//qcd_taujet_origin->Fill(abs( jet_origin->pdgId() ));
+					// qcd_jet_origin
+
+					// per-jet-origin distrs
+					if (jet_origin == 21) // gluons
+						fill_jet_distrs_1D_2D(channel + selection + ("_tau_jets_distr_g"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+					else if (jet_origin == 5) // b-quarks
+						fill_jet_distrs_1D_2D(channel + selection + ("_tau_jets_distr_b"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+					else if (jet_origin == 0) // other stuff (taus probably)
+						fill_jet_distrs_1D_2D(channel + selection + ("_tau_jets_distr_o"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+					else if (jet_origin == 15) // the matched to gen_taus (dR < 1) jets
+						fill_jet_distrs_1D_2D(channel + selection + ("_tau_jets_distr_t"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+					else // other (light) quarks
+						fill_jet_distrs_1D_2D(channel + selection + ("_tau_jets_distr_q"), event_weight, jet.pt(), jet.eta(), jet_radius(jet));
+					}
+				continue;
+				}
+			}
+		}
+
+	// return success
+	return 0;
+	}
 
 
 
